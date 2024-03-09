0ebd48490d4e97065f3b00ee2f89b9751a69e408
kehindeBankole <bankolek1@gmail.com>
2024-03-09 20:41:12 +0100

    Initial Commit

---
+++ SOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOS
+//
+//  Created by kehinde on 09/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOS/SOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSApp.swift
+//  SOS
+//
+//  Created by kehinde on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
