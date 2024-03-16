744a8dc65efe836c5781166f5e3636eaeb228f54
kehindeBankole <bankolek1@gmail.com>
2024-03-16 15:51:32 +0100

    list contacts

--- SOS/ContentView.swift
+++ SOS/ContentView.swift
@@ -7,37 +7,81 @@
 
 import SwiftUI
 import MapKit
-
+import Contacts
 
 struct ContentView: View {
-    
+    @State private var contacts : [CNContact] = []
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State private var isHelp = false
     
-    var body: some View {
-        GeometryReader{ reader in
-            ZStack(alignment: .trailing) {
-                Map(position: $position){
-                    UserAnnotation()
-                }.mapControls{
-                    MapUserLocationButton()
-                    MapPitchToggle()
+    func getContactList(){
+        let CNStore = CNContactStore()
+        
+        switch CNContactStore.authorizationStatus(for: .contacts){
+        case .authorized:
+            do{
+                let keys = [CNContactGivenNameKey as CNKeyDescriptor , CNContactFamilyNameKey as CNKeyDescriptor , CNContactPhoneNumbersKey as CNKeyDescriptor]
+                let request = CNContactFetchRequest(keysToFetch: keys)
+                try CNStore.enumerateContacts(with: request, usingBlock: { contact , _ in
+                    
+                    contacts.append(contact)
+                    
+                })
+            }catch{
+                
+            }
+        case .denied:
+            print("denined")
+        case .notDetermined:
+            CNStore.requestAccess(for: .contacts, completionHandler:{ granted , error in
+                if(granted){
+                    getContactList()
                 }
-                HStack{
-                    Button(action: {
-                      //do stuff
-                    }){
-                        Text("HELP").foregroundStyle(.white)
-                            .padding()
+            })
+        default:
+            print("do nothing...")
+        }
+        
+    }
+    var body: some View {
+        
+        ZStack(alignment: .trailing) {
+            Map(position: $position){
+                UserAnnotation()
+            }.mapControls{
+                MapUserLocationButton()
+                MapPitchToggle()
+            }
+            HStack{
+                Button(action: {
+                    
+                    isHelp.toggle()
+                    
+                }){
+                    Text("HELP").foregroundStyle(.white)
+                        .padding()
+                }
+            }.background(.red)
+                .cornerRadius(15)
+                .padding()
+        }
+        .sheet(isPresented: $isHelp){
+           List{
+                ForEach(Array(contacts.enumerated()), id: \.element.id){index ,  contactDetail in
+                    HStack{
+                        Text("\(contactDetail.givenName)")
+                        Text("\(contactDetail.phoneNumbers.first?.value.stringValue ?? "")")
                     }
-                }.background(.red)
-                    .cornerRadius(15)
-                    .padding()
-            }.onAppear{
+                    
+                    .padding(.vertical , 10)
+                }.presentationDetents([.medium , .large])
+            }
+        }.id(contacts)
+            .onAppear{
+                getContactList()
                 CLLocationManager().requestWhenInUseAuthorization()
             }
-
-        }
-
+        
     }
 }
 
